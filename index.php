ced15e3f23cbccf417e16abe88ac6f49354d4f59
Achu <achusivadasan416@gmail.com>
2024-04-05 16:41:09 +0530

    test3

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
                     <h2>Pharmacy Billing</h2>
                 </a>
                 <a href="offlineconsultation/login.php" class="dashboardListBox" >
-                    <h2>Offline Appointmen</h2>
+                    <h2>Offline Appointments</h2>
                 </a>
 				
                 <a href="milletsstore/login.php" class="dashboardListBox" >

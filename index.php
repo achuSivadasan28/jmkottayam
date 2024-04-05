5a52ebb49f636d9314dbf0eec299279d537524db
Achu <achusivadasan416@gmail.com>
2024-04-05 15:47:36 +0530

    test1

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
                 <a href="login.php" class="dashboardListBox">
                     <h2>Pharmacy Billing</h2>
                 </a>
-                <a href="offlineconsultation/login.php" class="dashboardListBox">
+                <a href="offlineconsultation/login.php" class="dashboardListBox" style="color:red">
                     <h2>Offline Appointments</h2>
                 </a>
 				
